05f7c616d32ead9a39b19c7938184daf4d9edf66
Anthony Ross <mross@onetwosee.com>
2015-11-18 12:11:48 -0500

    Change bamboo var names

--- scripts/notify.js
+++ scripts/notify.js
@@ -2,25 +2,25 @@
 module.exports = function( robot ) {
 
   // This command needs to be added as the final Task un a build job:
-  // curl -X POST -H "Content-Type: application/json" -d '{"room": "general", "planKey":"${bamboo.planKey}","buildNumber":"${bamboo.buildNumber}"}' ${bamboo.hubotUrl}/hubot/bamboo/result
+  // curl -X POST -H "Content-Type: application/json" -d '{"room": "general", "buildKey":"${bamboo.buildKey}","buildNumber":"${bamboo.buildNumber}"}' ${bamboo.hubotUrl}/hubot/bamboo/result
   // substitue "general" with a user or channel name (without @ or #)
   robot.router.post('/hubot/bamboo/result', function(req, res) {
 
     var data = req.body.payload != null ? JSON.parse(req.body.payload) : req.body;
     var room = data.room;
-    var planKey = data.planKey;
+    var buildKey = data.buildKey;
     var buildNumber = data.buildNumber;
     var host = process.env.BAMBOO_HOST;
     var port = process.env.BAMBOO_PORT;
     var user = process.env.BAMBOO_USER;
     var pass = process.env.BAMBOO_PASS;
 
-    if ( !room || !planKey || !buildNumber || !host || !port || !user || !pass ) {
+    if ( !room || !buildKey || !buildNumber || !host || !port || !user || !pass ) {
       return res.end( JSON.stringify( "FAIL" ) );
     }
 
     var auth = user + ':' + pass;
-    var url  = "http://" + host + ":"  + port + "/rest/api/latest/result/" + planKey + "/" + buildNumber + ".json";
+    var url  = "http://" + host + ":"  + port + "/rest/api/latest/result/" + buildKey + "/" + buildNumber + ".json";
 
     return robot.http( url )
       .query({ "os_authType": "basic" })
@@ -50,7 +50,7 @@ module.exports = function( robot ) {
             text: json.state,
             fallback: "Build Results: " + json.plan.name,
             pretext: json.plan.name + "\nhttp://" + host + ":" + port + "/browse/" + json.key,
-            color: json.state == "Successful" ? "good" : "danger",
+            color: (json.state == "Successful") ? "good" : (json.state == "Failed") ? "danger" : "warning",
             fields: fields
           }
         });
