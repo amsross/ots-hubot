7d4d1cb5c8e7fef0f12d2d775f90d04b40431e28
Dillon Shook <dshook@onetwosee.com>
2018-09-06 11:41:55 -0400

    Maybe a regex problem?

--- scripts/food.js
+++ scripts/food.js
@@ -1,11 +1,11 @@
 module.exports = function( robot ) {
 
   var phrases = [
-      '/giphy food'
-    ];
+    '/giphy food'
+  ];
 
-    robot.hear( /\b(where's the food)\b/i, function( res ) {
-      return res.send( res.random( phrases ) );
-    });
+  robot.hear( /\b(where'?s the food)\b/i, function( res ) {
+    return res.send( res.random( phrases ) );
+  });
 
-  };
+};
